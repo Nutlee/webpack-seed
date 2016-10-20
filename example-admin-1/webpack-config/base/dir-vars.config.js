f0cbbcc77a920a0cdd6da247de7d5e1c9cb5b69f
Array Huang <hyw125@qq.com>
2016-10-20 17:28:43 +0800

    调通

--- example-admin-1/webpack-config/base/dir-vars.config.js
+++ example-admin-1/webpack-config/base/dir-vars.config.js
@@ -1,25 +1,11 @@
 var path = require('path');
-var mE = {};
+var mE = require('../../../core/webpack-config/base/dir-vars.config.js');
 
-// 源文件目录
-mE.staticRootDir = path.resolve(__dirname, '../../../'); // 根目录
-
-/**/mE.vendorDir = path.resolve(mE.staticRootDir, './vendor'); // 存放所有不能用npm管理的第三方库
-
-/**/mE.coreDir = path.resolve(mE.staticRootDir, './core'); // 项目框架
-/**//**/mE.dllDir = path.resolve(mE.coreDir, './dll'); // 存放由各种不常改变的js/css打包而来的dll
-/**//**/mE.libsDir = path.resolve(mE.coreDir, './libs');  // 与业务逻辑无关的库都可以放到这里
-/**//**/mE.coreConfigDir = path.resolve(mE.coreDir, './config'); // 存放项目框架的各种配置文件
-/**//**/mE.componentsDir = path.resolve(mE.coreDir, './components'); // 存放组件，可以是纯HTML，也可以包含js/css/image等，看自己需要
-/**//**/mE.layoutDir = path.resolve(mE.coreDir, './layout'); // 存放UI布局，组织各个组件拼起来，因应需要可以有不同的布局套路
-
-/**/mE.srcRootDir = path.resolve(mE.staticRootDir, './example-admin'); // 项目业务代码根目录
+/**/mE.srcRootDir = path.resolve(mE.staticRootDir, './example-admin-1'); // 项目业务代码根目录
+/**//**/mE.buildDir = path.resolve(mE.srcRootDir, './build'); // 存放编译后生成的所有代码、资源（图片、字体等，虽然只是简单的从源目录迁移过来）
 /**//**/mE.pagesDir = path.resolve(mE.srcRootDir, './pages'); // 存放各个页面独有的部分，如入口文件、只有该页面使用到的css、模板文件等
 /**//**/mE.publicDir = path.resolve(mE.srcRootDir, './public-resource'); // 存放各个页面使用到的公共资源
 /**//**//**/mE.logicDir = path.resolve(mE.publicDir, './logic'); // 存放公用的业务逻辑
 /**//**//**/mE.configDir = path.resolve(mE.publicDir, './config'); // 存放项目的各种配置文件
-
-// 生成文件目录
-mE.buildDir = path.resolve(mE.staticRootDir, './build'); // 存放编译后生成的所有代码、资源（图片、字体等，虽然只是简单的从源目录迁移过来）
 
 module.exports = mE;
--- example-admin-1/webpack-config/base/page-entries.config.js
+++ example-admin-1/webpack-config/base/page-entries.config.js
@@ -5,4 +5,5 @@ var options = {
   sync: true, // 这里不能异步，只能同步
 };
 var globInstance = new glob.Glob('!(_)*/!(_)*', options); // 考虑到多个页面共用HTML等资源的情况，跳过以'_'开头的目录
+
 module.exports = globInstance.found; // 一个数组，形如['index/index', 'index/login', 'alert/index']
