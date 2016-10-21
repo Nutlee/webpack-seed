2f8a6245893aaf99e8417d0d7f52fd5ac11d2402
Nutlee <lixuanyy@gmail.com>
2016-10-21 21:13:57 +0800

    调整为 webpack-dev-server 热更新

--- webpack-config/output.config.js
+++ webpack-config/output.config.js
@@ -1,7 +1,9 @@
 var dirVars = require('./base/dir-vars.config.js');
 module.exports = {
   path: dirVars.buildDir,
-  publicPath: '../../',
+  // publicPath: '../../',
+  // publicPath: 'build/',
+  publicPath: 'http://192.168.2.66:8080/',
   filename: '[name]/entry.js',    // [name]表示entry每一项中的key，用以批量指定生成后文件的名称
   chunkFilename: '[id].bundle.js',
 };
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,34 @@ module.exports = {
 
   resolve: require('./webpack-config/resolve.config.js'),
 
-  plugins: require('./webpack-config/plugins.product.config.js'),
+  plugins: require('./webpack-config/plugins.dev.config.js'),
 
   eslint: require('./webpack-config/vendor/eslint.config.js'),
 
   postcss: require('./webpack-config/vendor/postcss.config.js'),
+  // devServer: {
+  //     contentBase: 'build/',  //以public为根目录提供文件
+  //     colors: true,
+  //     historyApiFallback: true,
+  //     inline: true,
+  //     host: '192.168.2.66',
+  //     port: 8080
+  // }
+  
+  // devServer: {
+  // 	proxy: {
+  // 	    '/api/a': {
+  // 	    	target: 'http://localhost:3000',
+  // 	    	secure: false,
+  // 	    	bypass: function(req, res, proxyOptions) {
+  // 	    		console.log(res);
+  // 	    	    if (req.headers.accept.indexOf('html') !== -1) {
+  // 	    	    	console.log(req.body);
+  // 	    	        console.log('Skipping proxy for browser request.');
+  // 	    	        res.send='HelloWorld';
+  // 	    	    }
+  // 	    	},
+  // 	    }
+  // 	}
+  // }
 };
